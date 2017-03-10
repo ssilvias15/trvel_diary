98827039d187c21960eafe40522a769d93b87cec
Silvia <ssilvias15@yahoo.com>
2017-03-10 19:33:53 +0000

    create reviews

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -70,6 +70,5 @@ class CitiesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
       params.fetch(:city, {}).permit(:name)
-      params.fetch(:city, {})
     end
 end
