250a8423f5a3383376d7d92ebd945011d65e52f4
Silvia <ssilvias15@yahoo.com>
2017-03-11 14:31:42 +0000

    image uploads

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,8 @@
 class City < ApplicationRecord
   has_many :reviews
+  has_attached_file :picture, styles: { validate_media_type: false, medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+
+  #validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  #validates_attachment_file_name :picture, :matches => [/png\Z/, /jpg\Z/, /gif\Z/]
+  do_not_validate_attachment_file_type :picture
 end
